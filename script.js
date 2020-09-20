5c36255f6ff045391f24c1e588171fcd93ef6219
chyppoli <claude.hyppolite@gmail.com>
2020-09-20 16:41:06 -0400

    format script.js with prettier

--- script.js
+++ script.js
@@ -16,8 +16,6 @@
 // THEN the password is either displayed in an alert or written to the page
 // ```
 
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -27,45 +25,44 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 //  write new function called generatePassword returning a string for the password
 
 // function to shuffle the final password to randomize the pattern
 function shuffle(string) {
   var a = string.split(""),
-      n = a.length;
+    n = a.length;
 
-    for (var i = n -1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var tmp = a[i];
-      a[i] = a[j];
-      a[j] = tmp;
-    }
+  for (var i = n - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+  }
   return a.join("");
-      
 }
 // function will generate password based on options of including uppercase/lowercase letters, numbers and special characters
-function generatePassword () {
-
-
+function generatePassword() {
   // function to force valid length at the very beginning
-  function askForPwdLen () {
-    var promptPwdLen = prompt("Please select a password length between 8 and 128?")
+  function askForPwdLen() {
+    var promptPwdLen = prompt(
+      "Please select a password length between 8 and 128?"
+    );
 
-    while (promptPwdLen  < 8 || promptPwdLen  > 128){
+    while (promptPwdLen < 8 || promptPwdLen > 128) {
       alert("Invalid password length Entered!");
-      promptPwdLen  = prompt("Please enter a valid password length not less than 8 and not greater than 128");
+      promptPwdLen = prompt(
+        "Please enter a valid password length not less than 8 and not greater than 128"
+      );
     }
-    return promptPwdLen ;
+    return promptPwdLen;
   }
-  
-  // function to handle password options 
+
+  // function to handle password options
 
   function askForPwdOptions() {
     var promptUpperCase = confirm("Should password include Uppercase Letters?");
@@ -73,55 +70,49 @@ function generatePassword () {
     var promptNbr = confirm("Should password include Numbers?");
     var promptSpecial = confirm("Should password include special characters?");
 
-    while (promptLowerCase + promptNbr + promptUpperCase + promptSpecial < 1 ) {
+    while (promptLowerCase + promptNbr + promptUpperCase + promptSpecial < 1) {
       alert("You must select at least one password option!");
-       promptUpperCase = confirm("Should password include Uppercase Letters?");
-       promptLowerCase = confirm("Should password include Lowercase Letters?");
-       promptNbr = confirm("Should password include Numbers?");
-       promptSpecial = confirm("Should password include special characters?");
+      promptUpperCase = confirm("Should password include Uppercase Letters?");
+      promptLowerCase = confirm("Should password include Lowercase Letters?");
+      promptNbr = confirm("Should password include Numbers?");
+      promptSpecial = confirm("Should password include special characters?");
     }
     return [promptUpperCase, promptLowerCase, promptNbr, promptSpecial];
   }
   // call functions to prompt, capture, validate then return pwd options and length
-  const pwdLen        = askForPwdLen();
-  var pwdOptions      = askForPwdOptions();
+  const pwdLen = askForPwdLen();
+  var pwdOptions = askForPwdOptions();
 
+  var inclUpperCase = pwdOptions[0];
+  var inclLowerCase = pwdOptions[1];
+  var inclNbr = pwdOptions[2];
+  var inclSpecial = pwdOptions[3];
 
-  var inclUpperCase   = pwdOptions[0];
-  var inclLowerCase   = pwdOptions[1];
-  var inclNbr         = pwdOptions[2];
-  var inclSpecial     = pwdOptions[3];
+  // section to determine how many times loop should run based on selected length and options
+  var newPassword = "";
+  var countOfConditions = inclUpperCase + inclLowerCase + inclNbr + inclSpecial;
+  var loopCount = Math.ceil(pwdLen / countOfConditions);
+  var finalPassword = "";
 
-
-// section to determine how many times loop should run based on selected length and options 
-var newPassword = "";
-var countOfConditions = inclUpperCase  + inclLowerCase + inclNbr + inclSpecial;
-var loopCount = Math.ceil( (pwdLen / countOfConditions)) ;
-var finalPassword = "";
-
-
-// loop to select random values based on ascii chars and append to password variable
-for (var i = 0; i < loopCount+1; i++ ) {
-  if (inclUpperCase === true && newPassword.length < pwdLen) {
-    newPassword += String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+  // loop to select random values based on ascii chars and append to password variable
+  for (var i = 0; i < loopCount + 1; i++) {
+    if (inclUpperCase === true && newPassword.length < pwdLen) {
+      newPassword += String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    }
+    if (inclLowerCase === true && newPassword.length < pwdLen) {
+      newPassword += String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    }
+    if (inclNbr === true && newPassword.length < pwdLen) {
+      newPassword += String.fromCharCode(Math.floor(Math.random() * 9 + 49));
+    }
+    if (inclSpecial === true && newPassword.length < pwdLen) {
+      newPassword += String.fromCharCode(Math.floor(Math.random() * 15 + 33));
+    }
   }
-  if (inclLowerCase === true && newPassword.length < pwdLen) {
-    newPassword += String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-  }
-    if (inclNbr === true && newPassword.length < pwdLen ) {
-    newPassword += String.fromCharCode(Math.floor(Math.random() * 9 + 49));
-  }
-  if (inclSpecial === true && newPassword.length < pwdLen) {
-    newPassword += String.fromCharCode(Math.floor(Math.random()*15 + 33));
-  }
+
+  //shuffle the values to further randomize
+  finalPassword = shuffle(newPassword);
+
+  // return the generated password
+  return finalPassword;
 }
-
-
- //shuffle the values to further randomize
- finalPassword = shuffle(newPassword);
- 
-// return the generated password
-return finalPassword;
-
-}
-
